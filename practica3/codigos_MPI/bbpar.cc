2c76499ed229a4548e580a071c3d28ec182503d5
juanfrandm98 <juanfrandm98@gmail.com>
2020-06-01 12:13:28 +0200

    Práctica 3 - añadido código de resolución inicial y compilación

---
+++ practica3/codigos_MPI/bbpar.cc
@@ -0,0 +1,19 @@
+/* ******************************************************************** */
+/*               Algoritmo Branch-And-Bound Paralelo                    */
+/*                  Juan Francisco Díaz Moreno                          */
+/*                  Junio de 2020                                       */
+/* ******************************************************************** */
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <mpi.h>
+#include "libbb.h"
+
+using namespace std;
+
+unsigned int NCIUDADES;
+int rank, size;
+
+int main( int argc, char **argv[] ) {
+  
+}
